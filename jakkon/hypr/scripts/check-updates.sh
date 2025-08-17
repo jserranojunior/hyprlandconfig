90c2ada9602132f9021217fe8099c5558bc9136f
Jorge S junior <jorgeserranojunior@gmail.com>
2025-08-17 12:09:17 -0300

    Instalados e limpeza

--- jakkon/hypr/scripts/check-updates.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-updates=$(dnf check-update -q 2>/dev/null | grep -v "^$" | wc -l)
-if [ "$updates" -gt 0 ]; then
-    echo '{"text": "* '$updates'", "tooltip": "Atualizações disponíveis"}'
-else
-    echo '{"text": "0", "tooltip": "Sistema atualizado"}'
-fi
